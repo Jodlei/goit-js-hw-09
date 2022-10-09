e8078b91287557815baa8d734e28e6b7be28edbe
Yurii PErekrestnyi <jodlei1618@gmail.com>
2022-10-10 00:56:58 +0300

    3 update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,25 +2,24 @@ const form = document.querySelector('.form');
 
 form.addEventListener('submit', onFormSubmit);
 
-function onFormSubmit(evt) {
-  evt.peventDefault();
+function onFormSubmit(e) {
+  e.preventDefault();
 
-  let elDelay = Number(evt.currentTarget.delay.value);
-  const elStep = Number(evt.currentTarget.step.value);
-  const elAmount = Number(evt.currentTarget.amount.value);
+  let delay = Number(e.currentTarget.delay.value);
+  const step = Number(e.currentTarget.step.value);
+  const amount = Number(e.currentTarget.amount.value);
 
-  for (let position = 1; position <= elAmount; position += 1) {
-    createPromise(position, elDelay)
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
       .then(({ position, delay }) => {
         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-    elDelay += elStep;
+    delay += step;
   }
 }
-
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
